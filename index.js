b1dc6ed2100c8ec887d0a2bf76c6b4c70f0778d1
slawoe <info@slawomirernst.de>
2021-05-24 17:13:53 +0200

    Add snake grid create function

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const grid = document.querySelector(".grid");
-const startButton = document.querySelector(".#start");
-const score = document.querySelector(".#score");
+const startButton = document.querySelector("#start");
+const score = document.querySelector("#score");
+let squares = [];
+
+function gridCreator() {
+  for (let i = 0; i < 100; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    grid.appendChild(square);
+    squares.push(square);
+  }
+}
+
+gridCreator();
+console.log(squares.length);
